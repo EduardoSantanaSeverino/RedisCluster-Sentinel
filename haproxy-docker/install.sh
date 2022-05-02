322c4c3aa00925689a52d29e23c894460b453585
EduardoSantanaSeverino <eduardoamparos@gmail.com>
2022-05-02 12:48:22 -0400

    Update haproxy

--- haproxy-docker/install.sh
+++ haproxy-docker/install.sh
@@ -16,66 +16,33 @@ echo ' Installing docker-compose - Start'
 sudo apt -y install docker-compose
 echo ' Installing docker-compose - End'
 
+echo ' Installing jq - Start'
+sudo apt -y install jq
+echo ' Installing jq - End'
+
 cd haproxy-docker
 
-echo "" > .env
-
-# pass the REDIS_A_HOST_IP as 1
-if ["$1"] then
+# pass the NODES as 1
+if ["$1"] 
+then
     echo "Argument supplied is: $1"
-    echo "REDIS_A_HOST_IP=$1" >> .env
+    NODES=$1
 else
-    echo "NO Argument supplied FOR REDIS_A_HOST_IP."
-    echo "REDIS_A_HOST_IP=192.168.22.131" >> .env
+    echo "NO Argument supplied FOR NODES."
+    NODES="[ '192.168.22.131','192.168.22.132','192.168.22.133' ]"
 fi
 
-# pass the REDIS_B_HOST_IP as 2
-if ["$2"] then
-    echo "Argument supplied is: $2"
-    echo "REDIS_B_HOST_IP=$2" >> .env
-else
-    echo "NO Argument supplied FOR REDIS_B_HOST_IP."
-    echo "REDIS_B_HOST_IP=192.168.22.132" >> .env
-fi
-
-# pass the REDIS_C_HOST_IP as 3
-if ["$3"] then
-    echo "Argument supplied is: $3"
-    echo "REDIS_C_HOST_IP=$3" >> .env
-else
-    echo "NO Argument supplied FOR REDIS_C_HOST_IP."
-    echo "REDIS_C_HOST_IP=192.168.22.133" >> .env
-fi
-
-
-# Load the env variables.
-set -o allexport
-[[ -f .env ]] && source .env
-set +o allexport
-
-# Replace the Sorry Cypress project name.
-sed -i -e "s|REDIS_MASTER|$REDIS_MASTER|g" sentinel.conf
-sed -i -e "s|REDIS_MASTER|$REDIS_MASTER|g" redis.conf
-
-SUB='0 received,'
 COUNTER=1
 
-while [ $COUNTER -lt 60 ];
-do 
-    sleep 2
-    PING_RESULTS=$(ping $REDIS_MASTER -c 1)
+# Replace single quotes with double.
+NODES=${NODES//[\']/\"}
 
-    if [[ "$PING_RESULTS" == *"$SUB"* ]]; then
-        echo "------------------------------------------------------------------------"
-        echo "[INFO] $SUB WAS FOUND." 
-        echo "------------------------------------------------------------------------"
-        COUNTER=60
-    else
-        echo "------------------------------------------------------------------------"
-        echo "[ERROR] NOT FOUND THE $SUB, ON THE CONTENT, WAITING." 
-        echo "------------------------------------------------------------------------"
-        COUNTER=`expr $COUNTER + 1`
-    fi
+cat haproxy.conf > haproxy.conf.temp
+
+for row in $(echo "${NODES}" | jq -r '.[]'); do
+    echo $row
+    echo "	server redis-$COUNTER $row:6379 check inter 1s" >> haproxy.conf.temp
+    COUNTER=`expr $COUNTER + 1`
 done
 
 echo ' docker-compose up -d - Start'
